36d7449eedd68bd095aabbe28312e17970ccb23a
DuyNH <hoangduy791999@gmail.com>
2021-12-11 08:28:15 +0700

    Laracast episode 1-9

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+</head>
+
+<body>
+    <h1>My Blog</h1>
+    <article>
+        <h1><a href="/posts/my-first-post">First post</a></h1>
+        <p>Officia id id non quis ex fugiat ea quis aliqua proident incididunt. Esse sunt adipisicing incididunt ut velit qui. Consectetur veniam ex et laboris ipsum sunt nisi eiusmod id veniam commodo aliqua id. Officia adipisicing eiusmod et ex duis qui commodo. Sunt consectetur sit est anim cupidatat commodo ullamco. Cillum ullamco ex amet aliquip qui. Qui non ullamco fugiat Lorem mollit Lorem culpa elit incididunt commodo et voluptate.</p>
+
+
+    </article>
+    <article>
+
+        <h1><a href="/posts/my-second-post">Second post</a></h1>
+        <p>Officia id id non quis ex fugiat ea quis aliqua proident incididunt. Esse sunt adipisicing incididunt ut velit qui. Consectetur veniam ex et laboris ipsum sunt nisi eiusmod id veniam commodo aliqua id. Officia adipisicing eiusmod et ex duis qui commodo. Sunt consectetur sit est anim cupidatat commodo ullamco. Cillum ullamco ex amet aliquip qui. Qui non ullamco fugiat Lorem mollit Lorem culpa elit incididunt commodo et voluptate.</p>
+
+    </article>
+    <article>
+
+        <h1><a href="/posts/my-third-post">Third post</a></h1>
+        <p>Officia id id non quis ex fugiat ea quis aliqua proident incididunt. Esse sunt adipisicing incididunt ut velit qui. Consectetur veniam ex et laboris ipsum sunt nisi eiusmod id veniam commodo aliqua id. Officia adipisicing eiusmod et ex duis qui commodo. Sunt consectetur sit est anim cupidatat commodo ullamco. Cillum ullamco ex amet aliquip qui. Qui non ullamco fugiat Lorem mollit Lorem culpa elit incididunt commodo et voluptate.</p>
+
+    </article>
+</body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('posts');
+});
+
+Route::get('posts/{post}', function ($slug) {
+
+    $path = __DIR__ . "/../resources/posts/{$slug}.html";
+
+
+    if (!file_exists($path)) {
+        // ddd('file does not exist'); // dump die debug
+        // abort(404);
+        return redirect('/');
+    }
+
+    $post = file_get_contents($path);
+
+    return view('post', [
+        // 'post' => '<h1>Hello World</h1>' // $post
+        'post' => $post
+    ]);
+})->where('post', '[A-z_\-]+'); // Regular expression constraints
