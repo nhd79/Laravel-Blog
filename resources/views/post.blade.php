5966c6cbf9ee5e6b41d8e24c9a7ea428a8f65319
DuyNH <hoangduy791999@gmail.com>
2021-12-13 22:16:58 +0700

    15-Blade layout template inheritance

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,16 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layout')
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Blog</title>
-    <link rel="stylesheet" href="/app.css">
 
-</head>
-
-<body>
+@section('content')
     <article>
         <h1>
             {{ $post->title }}
@@ -20,6 +11,4 @@
         </div>
     </article>
     <a href="/">Go back</a>
-</body>
-
-</html>
+@endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,16 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layout')
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Blog</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
+@section('banner')
+    <h1>My Blog</h1>
+@endsection
 
-<body>
-    <!-- <h1>My Blog</h1> -->
+@section('content')
     @foreach ($posts as $post)
         {{-- @dd($loop) --}}
         <article class="{{ $loop->even ? 'even' : '' }}">
@@ -24,6 +18,5 @@
             </div>
         </article>
     @endforeach
-</body>
 
-</html>
+@endsection
