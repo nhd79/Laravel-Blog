012cdb327e2a37e002a05c9d15b38453294d5e64
DuyNH <hoangduy791999@gmail.com>
2021-12-13 09:29:00 +0700

    14-3:24

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -14,7 +14,7 @@
     <article>
         <h1><?= $post->title; ?></h1>
 
-        <div><?= $post->body; ?></div>
+        <div>{!! $post->body !!}</div>
     </article>
     <a href="/">Go back</a>
 </body>
