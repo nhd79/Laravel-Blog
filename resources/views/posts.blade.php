e0aecb7b706b8611c40f7721a0dacd289a831ffb
DuyNH <hoangduy791999@gmail.com>
2021-12-25 14:41:37 +0700

    23-Route Model Binding

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,7 +3,7 @@
         {{-- @dd($loop) --}}
         <article class="{{ $loop->even ? 'even' : '' }}">
             <h1>
-                <a href="/posts/{{ $post->id }}">
+                <a href="/posts/{{ $post->slug }}">
                     {!! $post->title !!}
                 </a>
             </h1>
