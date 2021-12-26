50a8992574fc4030eb2ab7685c7d36ce12e8ae0b
DuyNH <hoangduy791999@gmail.com>
2021-12-26 15:10:56 +0700

    25-Show all posts in a category

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 
 use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,5 +25,11 @@ Route::get('/', function () {
 Route::get('posts/{post:slug}', function (Post $post) {
     return view('post', [
         'post' => $post
+    ]);
+});
+
+Route::get('categories/{category:slug}', function (Category $category) {
+    return view('posts', [
+        'posts' => $category->posts
     ]);
 });
