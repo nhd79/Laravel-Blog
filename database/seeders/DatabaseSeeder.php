0a41ce8a9ca37ce4e7e6007d34364af3dceb75b0
{Duy} <hoangduy791999@gmail.com>
2021-12-27 20:09:44 +0700

    28-Factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,43 +16,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Post::truncate();
-        Category::truncate();
-
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
+        $user = User::factory()->create([
+            'name' => 'Duy Nguyen'
         ]);
 
-        $family  = Category::create([
-            'name' => 'Family',
-            'slug' => 'family'
-        ]);
-
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $family->id,
-            'title' => 'My Family Post',
-            'slug' => 'my-family-post',
-            'excerpt' => '<p>Lorem ipsum dolor sit amet.</p>',
-            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni exercitationem consectetur nesciunt voluptatum sequi recusandae ab architecto sunt quam eaque?</p>'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My Work Post',
-            'slug' => 'my-work-post',
-            'excerpt' => '<p>Lorem ipsum dolor sit amet.</p>',
-            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni exercitationem consectetur nesciunt voluptatum sequi recusandae ab architecto sunt quam eaque?</p>'
+        Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
     }
 }
