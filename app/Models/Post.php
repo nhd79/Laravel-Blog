7aeeaaeb9823179c9d280628101612246dc87e31
{Duy} <hoangduy791999@gmail.com>
2021-12-30 19:09:55 +0700

    38-Search cleaner way: controller + filter

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -15,6 +15,17 @@ class Post extends Model
 
     protected $with = ['category', 'author'];
 
+    public function scopeFilter($query, array $filters)
+    {
+        $query->when(
+            $filters['search'] ?? false,
+            fn ($query, $search) =>
+            $query
+                ->where('title', 'like', '%' . $search . '%')
+                ->orWhere('excerpt', 'like', '%' . $search . '%')
+        );
+    }
+
     public function category()
     {
         // hasOne, hasMany, belongsTo, belongsToMany
