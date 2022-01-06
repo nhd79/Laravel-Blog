1bc1a46dc9295d14d7878fc406c2b0cabb74438a
DuyNH <hoangduy791999@gmail.com>
2022-01-06 15:14:45 +0700

    46-Automatic Password Hashing With Mutators

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -18,7 +18,6 @@ class RegisterController extends Controller
             'username' => 'required|max:255|min:3',
             'email' => 'required|email|max:255',
             'password' => 'required|min:7|max:255'
-            // 'password' => ['required','min:7','max:255']
         ]);
 
         User::create($attributes);
